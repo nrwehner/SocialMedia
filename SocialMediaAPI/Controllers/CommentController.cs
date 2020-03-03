08a26c776d7d3f7ac163237a7fcf0ea4ac686f49
Andrea-Kimball <aundreya.7@gmail.com>
2020-03-03 11:33:19 -0500

    First commit

--- SocialMediaAPI/Controllers/CommentController.cs
+++ SocialMediaAPI/Controllers/CommentController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Models;
+using Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,11 +10,46 @@ using System.Web.Http;
 
 namespace SocialMediaAPI.Controllers
 {
+    [Authorize]
     public class CommentController : ApiController
     {
-        //public IHttpActionResult Get()
+        //GET ALL
+        public IHttpActionResult Get()
+        {
+            CommentService commentService = CreateCommentService();
+            var comments = commentService.GetComment();
+            return Ok(comments);
+        }
+        //POST METHOD
+        public IHttpActionResult Post(CommentCreate comment)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateCommentService();
+
+            if (!service.CreateComment(comment))
+                return InternalServerError();
+
+            return Ok();
+        }
+        private CommentService CreateCommentService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var commentService = new CommentService(userId);
+            return commentService;
+        }
+
+
+        //UPDATE METHOD- WOULD REQUIRE CHANGES TO THE COMMENTSERVICE CLASS
+        //public IHttpActionResult Put(CommentEdit comment)
         //{
-            
+        //    if (!ModelState.IsValid)
+        //        return BadRequest(ModelState);
+        //    var service = CreateCommentService();
+
+        //    if (!service.UpdateComment(comment))
+        //        return InternalServerError();
+        //    return Ok();
         //}
     }
 }
