2d05d54ed0db3149c81ac47e802afc7782196ac0
G Mason <gmason0702@gmail.com>
2020-03-03 11:54:05 -0500

    fifthcommit

--- Models/ReplyListItem.cs
+++ Models/ReplyListItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    class ReplyListItem
+    public class ReplyListItem
     {
+        public string Text { get; set; }
+
+        public User Name { get; set; }
+        public virtual User Author { get; set; }
+        public Post CommentPost { get; set; }
+        public Comment ReplyComment { get; set; }
     }
 }
--- SocialMediaAPI/Controllers/ReplyController.cs
+++ SocialMediaAPI/Controllers/ReplyController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Models;
+using Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +12,38 @@ namespace SocialMediaAPI.Controllers
 {
     public class ReplyController : ApiController
     {
+        public IHttpActionResult Get()
+        {
+            ReplyService replyService = CreateReplyService();
+            var replies = replyService.GetReplies();
+            return Ok(replies);
+        }
+
+        public IHttpActionResult Post(ReplyCreate reply)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateReplyService();
+
+            if (!service.CreateReply(reply))
+                return InternalServerError();
+
+            return Ok();
+
+        }
+
+
+
+
+
+
+        private ReplyService CreateReplyService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var replyService = new ReplyService(userId);
+            return replyService;
+
+        }
     }
 }
